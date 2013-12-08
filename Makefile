4c5946bf8fd4c240a9088d12a24e51ee5ae4ef10
Miklos Vajna <vmiklos@collabora.co.uk>
2013-12-08 17:08:27 +0100

    Import inlineize
    It allows to create a self-contained svg from a dot file.

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
 test.png: test.dot
 	dot -Tpng -o test.png test.dot
 
+test.svg: test-noinline.svg inlineize.py
+	./inlineize.py test-noinline.svg test.svg
+
+test-noinline.svg: test.dot
+	dot -Tsvg -o test-noinline.svg test.dot
+
 test.dot: test.ged ged2dot.py ged2dotrc Makefile
 	./ged2dot.py > test.dot
 
 check:
 	cd test && ./test.py
-	pep8 --ignore=E501 ged2dot.py test/test.py
+	pep8 --ignore=E501 ged2dot.py inlineize.py test/test.py
 
 # In case ged2dotrc or test.dot is missing, create a copy based on the
 # screenshot sample.
---
+++ inlineize.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+import base64
+import sys
+import xml.etree.ElementTree as ElementTree
+
+namespaces = {
+    'svg': 'http://www.w3.org/2000/svg',
+    'xlink': 'http://www.w3.org/1999/xlink'
+}
+
+
+def inlineize(fro, to):
+    ElementTree.register_namespace('', namespaces['svg'])
+    ElementTree.register_namespace('xlink', namespaces['xlink'])
+    tree = ElementTree.ElementTree()
+    tree.parse(fro)
+    for image in tree.findall('.//{%s}image' % namespaces['svg']):
+        xlinkhref = '{%s}href' % namespaces['xlink']
+        href = image.attrib[xlinkhref]
+        sock = open(href, 'rb')
+        image.attrib[xlinkhref] = "data:image/png;base64,%s" % base64.b64encode(sock.read()).decode('ascii')
+        sock.close()
+    tree.write(to)
+
+
+def main():
+    inlineize(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
