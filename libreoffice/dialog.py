f0b9847b66dff5d3cb33fa510704101efc1ffbf1
Miklos Vajna <vmiklos@collabora.co.uk>
2013-12-25 12:06:56 +0100

    lo: GedcomDialog::__createControl() cleanup

--- libreoffice/dialog.py
+++ libreoffice/dialog.py
@@ -49,30 +49,30 @@ class GedcomDialog(unohelper.Base, XPropertyAccess, XExecutableDialog, XImporter
 
     def __createControl(self, xParent, type, id, tabIndex, left, top, width, height,
                         value=None, buttonType=None):
-        control = xParent.createInstance(type)
+        control = xParent.createInstance("com.sun.star.awt.UnoControl%sModel" % type)
         control.PositionX = left
         control.PositionY = top
         control.Width = width
         control.Height = height
         control.Name = id
         control.TabIndex = tabIndex
-        if type == "com.sun.star.awt.UnoControlFixedTextModel":
+        if type == "FixedText":
             control.Label = value
-        elif type == "com.sun.star.awt.UnoControlButtonModel":
+        elif type == "Button":
             control.PushButtonType = buttonType
             control.DefaultButton = buttonType == PushButtonType_OK
-        elif type == "com.sun.star.awt.UnoControlListBoxModel":
+        elif type == "ListBox":
             control.Dropdown = True
             # TODO check if we could make item text and data independent
             control.StringItemList = tuple(sorted(self.familyDict.keys(), key=lambda i: int(i.split(' (')[0][1:])))
             # Select the first item.
             control.SelectedItems = tuple([0])
-        elif type == "com.sun.star.awt.UnoControlNumericFieldModel":
+        elif type == "NumericField":
             control.Spin = True
             control.Value = ged2dot.Config.layoutMaxDepthDefault
             control.DecimalAccuracy = 0
             control.ValueMin = 0
-        elif type == "com.sun.star.awt.UnoControlCheckBoxModel":
+        elif type == "CheckBox":
             control.Label = value
             control.State = 1
         xParent.insertByName(id, control)
@@ -93,22 +93,14 @@ class GedcomDialog(unohelper.Base, XPropertyAccess, XExecutableDialog, XImporter
         xDialogModel.Title = "GEDCOM Import"
 
         # Then the model of the controls.
-        ftRootFamily = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlFixedTextModel",
-                                            id="ftRootFamily", tabIndex=0, left=10, top=10, width=100, height=10, value="Root family")
-        lbRootFamily = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlListBoxModel",
-                                            id="lbRootFamily", tabIndex=1, left=120, top=10, width=100, height=10)
-        ftLayoutMax = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlFixedTextModel",
-                                           id="ftLayoutMax", tabIndex=2, left=10, top=30, width=100, height=10, value="Number of generations to show")
-        nfLayoutMax = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlNumericFieldModel",
-                                           id="nfLayoutMax", tabIndex=3, left=120, top=30, width=100, height=10)
-        ftNameOrder = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlFixedTextModel",
-                                           id="ftNameOrder", tabIndex=4, left=10, top=50, width=100, height=10, value="Name order")
-        cbNameOrder = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlCheckBoxModel",
-                                           id="cbNameOrder", tabIndex=5, left=120, top=50, width=100, height=10, value="Forename first")
-        btnOk = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlButtonModel",
-                                     id="btnOk", tabIndex=6, left=110, top=70, width=50, height=10, buttonType=PushButtonType_OK)
-        btnCancel = self.__createControl(xDialogModel, type="com.sun.star.awt.UnoControlButtonModel",
-                                         id="btnCancel", tabIndex=7, left=170, top=70, width=50, height=10, buttonType=PushButtonType_CANCEL)
+        ftRootFamily = self.__createControl(xDialogModel, type="FixedText", id="ftRootFamily", tabIndex=0, left=10, top=10, width=100, height=10, value="Root family")
+        lbRootFamily = self.__createControl(xDialogModel, type="ListBox", id="lbRootFamily", tabIndex=1, left=120, top=10, width=100, height=10)
+        ftLayoutMax = self.__createControl(xDialogModel, type="FixedText", id="ftLayoutMax", tabIndex=2, left=10, top=30, width=100, height=10, value="Number of generations to show")
+        nfLayoutMax = self.__createControl(xDialogModel, type="NumericField", id="nfLayoutMax", tabIndex=3, left=120, top=30, width=100, height=10)
+        ftNameOrder = self.__createControl(xDialogModel, type="FixedText", id="ftNameOrder", tabIndex=4, left=10, top=50, width=100, height=10, value="Name order")
+        cbNameOrder = self.__createControl(xDialogModel, type="CheckBox", id="cbNameOrder", tabIndex=5, left=120, top=50, width=100, height=10, value="Forename first")
+        btnOk = self.__createControl(xDialogModel, type="Button", id="btnOk", tabIndex=6, left=110, top=70, width=50, height=10, buttonType=PushButtonType_OK)
+        btnCancel = self.__createControl(xDialogModel, type="Button", id="btnCancel", tabIndex=7, left=170, top=70, width=50, height=10, buttonType=PushButtonType_CANCEL)
 
         # Finally show the dialog.
         xDialog = self.createUnoService("com.sun.star.awt.UnoControlDialog")
