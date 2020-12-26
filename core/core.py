507f3eb88b37b9b3b7e07764b4dd096080ee954e
Miklos Vajna <vmiklos@vmiklos.hu>
2020-12-26 16:01:16 +0100

    ged2dot core: improve individual labels, add colors
    Also start adding type hints, not complete.
    
    Change-Id: I3f56ad98968d156ba45e57e0dca1c12749944c15

--- core/core.py
+++ core/core.py
@@ -7,23 +7,37 @@
 
 """A version of ged2dot that uses breadth-first search to traverse the gedcom graph."""
 
-def graph_find(graph, identifier):
+import os
+from typing import cast
+from typing import List
+from typing import Optional
+
+
+class Node:
+    def get_identifier(self) -> str:
+        ...
+
+    def get_depth(self) -> int:
+        ...
+
+
+def graph_find(graph: List[Node], identifier: str) -> Optional[Node]:
     if not identifier:
         return None
 
-    results = [node for node in graph if node.identifier == identifier]
+    results = [node for node in graph if node.get_identifier() == identifier]
     assert len(results) == 1
     return results[0]
 
 
-class Individual:
+class Individual(Node):
     """An individual is always a child in a family, and is an adult in 0..* families."""
-    def __init__(self):
+    def __init__(self) -> None:
         self.identifier = ""
         self.famc_id = ""
-        self.famc = None
-        self.fams_ids = []
-        self.fams_list = []
+        self.famc: Optional[Family] = None
+        self.fams_ids: List[str] = []
+        self.fams_list: List["Family"] = []
         self.depth = 0
         self.forename = ""
         self.surname = ""
@@ -31,40 +45,49 @@ class Individual:
         self.birth = ""
         self.death = ""
 
-    def resolve(self, graph):
-        self.famc = graph_find(graph, self.famc_id)
+    def resolve(self, graph: List[Node]) -> None:
+        self.famc = cast(Optional["Family"], graph_find(graph, self.famc_id))
         for fams_id in self.fams_ids:
-            self.fams_list.append(graph_find(graph, fams_id))
+            fams = graph_find(graph, fams_id)
+            assert fams
+            self.fams_list.append(cast("Family", fams))
 
-    def get_neighbours(self):
-        ret = []
+    def get_neighbours(self) -> List[Node]:
+        ret: List[Node] = []
         if self.famc:
             ret.append(self.famc)
         ret += self.fams_list
         return ret
 
+    def get_identifier(self) -> str:
+        return self.identifier
 
-class Family:
+    def get_depth(self) -> int:
+        return self.depth
+
+
+class Family(Node):
     """Family has exactly one wife and husband, 0..* children."""
-    def __init__(self):
+    def __init__(self) -> None:
         self.identifier = ""
         self.wife_id = ""
-        self.wife = None
+        self.wife: Optional["Individual"] = None
         self.husb_id = ""
-        self.husb = None
-        self.child_ids = []
-        self.children = []
-        self.child_list = []
+        self.husb: Optional["Individual"] = None
+        self.child_ids: List[str] = []
+        self.child_list: List["Individual"] = []
         self.depth = 0
 
-    def resolve(self, graph):
-        self.wife = graph_find(graph, self.wife_id)
-        self.husb = graph_find(graph, self.husb_id)
+    def resolve(self, graph: List[Node]) -> None:
+        self.wife = cast(Optional["Individual"], graph_find(graph, self.wife_id))
+        self.husb = cast(Optional["Individual"], graph_find(graph, self.husb_id))
         for child_id in self.child_ids:
-            self.child_list.append(graph_find(graph, child_id))
+            child = graph_find(graph, child_id)
+            assert child
+            self.child_list.append(cast("Individual", child))
 
-    def get_neighbours(self):
-        ret = []
+    def get_neighbours(self) -> List[Node]:
+        ret: List[Node] = []
         if self.wife:
             ret.append(self.wife)
         if self.husb:
@@ -72,12 +95,18 @@ class Family:
         ret += self.child_list
         return ret
 
+    def get_identifier(self) -> str:
+        return self.identifier
 
-def import_gedcom():
+    def get_depth(self) -> int:
+        return self.depth
+
+
+def import_gedcom() -> List[Node]:
     stream = open("test.ged", "rb")
     individual = None
     family = None
-    graph = []
+    graph: List[Node] = []
     in_birt = False
     in_deat = False
     for line_bytes in stream.readlines():
@@ -120,7 +149,10 @@ def import_gedcom():
                 if len(tokens) > 1:
                     individual.surname = tokens[1].strip()
             elif rest.startswith("FAMC") and individual:
-                individual.famc_id = rest[6:-1]
+                # At least <https://www.ancestry.com> sometimes writes multiple FAMC, which doesn't
+                # make sense. Import only the first one.
+                if not individual.famc_id:
+                    individual.famc_id = rest[6:-1]
             elif rest.startswith("FAMS") and individual:
                 individual.fams_ids.append(rest[6:-1])
             elif rest.startswith("BIRT"):
@@ -137,16 +169,16 @@ def import_gedcom():
             if rest.startswith("DATE") and individual:
                 year = rest.split(' ')[-1]
                 if in_birt:
-                    individual.birt = year
+                    individual.birth = year
                 elif in_deat:
                     individual.death = year
     return graph
 
 
-def bfs(graph, root):
+def bfs(graph: List[Node], root: Node) -> List[Node]:
     visited = [root]
     queue = [root]
-    ret = []
+    ret: List[Node] = []
 
     while queue:
         node = queue.pop(0)
@@ -172,7 +204,24 @@ def export_dot(subgraph):
         for node in subgraph:
             if not node.identifier.startswith("P"):
                 continue
-            stream.write(node.identifier + " [shape=box, label=\"" + node.surname + " " + node.forename + "\"];\n")
+            stream.write(node.identifier + " [shape=box, ")
+
+            image_path = "images/" + node.forename + " " + node.surname + " " + node.birth + ".jpg"
+            if not os.path.exists(image_path):
+                if node.sex:
+                    sex = node.sex.lower()
+                else:
+                    sex = 'u'
+                image_path = os.path.join("..", "placeholder-%s.png" % sex)
+            stream.write("label = <<table border=\"0\" cellborder=\"0\"><tr><td><img src=\"" + image_path + "\"/></td></tr><tr><td>" + node.surname + "<br/>" + node.forename + "<br/>" + node.birth + "-" + node.death + "</td></tr></table>>\n")
+
+            if node.sex is None:
+                sex = 'U'
+            else:
+                sex = node.sex.upper()
+            color = {'M': 'blue', 'F': 'pink', 'U': 'black'}[sex]
+
+            stream.write("color = " + color + "];\n")
         stream.write("\n")
 
         for node in subgraph:
