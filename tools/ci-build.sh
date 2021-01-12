b90bd59e465788fded9f11c222b9f2a1f3d36bd7
Miklos Vajna <vmiklos@vmiklos.hu>
2021-01-12 18:04:19 +0100

    CI: add a macOS builder
    This will be useful later when it builds a .dmg for releases.
    
    Change-Id: Ifd9f992e7d2c74f126788ed16cd4e71d09956ec2

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -6,10 +6,15 @@
 #
 
 #
-# This script runs all the tests for CI purposes.
+# Baseline: Ubuntu latest and macOS 10.15.
 #
 
 pip install -r requirements.txt
+
+if [ -n "${GITHUB_JOB}" -a "$(uname -s)" == "Darwin" ]; then
+    brew install graphviz
+fi
+
 make -j$(getconf _NPROCESSORS_ONLN) check
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
