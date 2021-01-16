4f172471cad4251f39f9810ee103eac08948a3e3
Miklos Vajna <vmiklos@vmiklos.hu>
2021-01-16 15:26:28 +0100

    qged2dot: fix up windows packaging of dot
    Still somewhat manual, but at least include enough dependencies so that
    the png output of dot starts to work.
    
    Change-Id: Idd0fe03e9f64a4fa46757d17304cb3c056253a5b

--- tools/pack.bat
+++ tools/pack.bat
@@ -5,11 +5,6 @@ pyinstaller ^
     --add-data="placeholder-f.png;." ^
     --add-data="placeholder-m.png;." ^
     --add-data="placeholder-u.png;." ^
-    --add-binary="graphviz/bin/cdt.dll;." ^
-    --add-binary="graphviz/bin/cgraph.dll;." ^
     --add-binary="graphviz/bin/dot.exe;." ^
-    --add-binary="graphviz/bin/expat.dll;." ^
-    --add-binary="graphviz/bin/gvc.dll;." ^
-    --add-binary="graphviz/bin/pathplan.dll;." ^
-    --add-binary="graphviz/bin/xdot.dll;." ^
     qged2dot.py
+copy /y graphviz\bin\*.* dist\qged2dot
