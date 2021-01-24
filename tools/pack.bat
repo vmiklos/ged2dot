727bf3a01cd6854247b6e507034a135b09c4696a
Miklos Vajna <vmiklos@vmiklos.hu>
2021-01-24 22:33:36 +0100

    qged2dot: give up on bundling dot for now
    Similar to macOS, let the platform package manager install it instead.
    
    This way pyinstaller now produces a working installation set, just need
    to zip it up, which is a manual action for now.
    
    Change-Id: Ia6d6c87a78fd09bb8574b6557abfc8187caf999c

--- tools/pack.bat
+++ tools/pack.bat
@@ -5,6 +5,4 @@ pyinstaller ^
     --add-data="placeholder-f.png;." ^
     --add-data="placeholder-m.png;." ^
     --add-data="placeholder-u.png;." ^
-    --add-binary="graphviz/bin/dot.exe;." ^
     qged2dot.py
-copy /y graphviz\bin\*.* dist\qged2dot
