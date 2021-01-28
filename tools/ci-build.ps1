b26d200ea9c0e19e56938f9dc493e27d0cfe8d9c
Miklos Vajna <vmiklos@vmiklos.hu>
2021-01-28 09:04:44 +0100

    qged2dot, windows: install wixtools/msicreator during CI
    Change-Id: I8d4593432fcf80d5654de42106cf2d60228887c0

--- tools/ci-build.ps1
+++ tools/ci-build.ps1
@@ -8,6 +8,16 @@
 # Baseline: Windows 2019
 #
 
+choco install wixtools
+
+cd tools
+git clone https://github.com/jpakkane/msicreator
+cd msicreator
+# Allow to add extra attributes to MajorUpgrade node. (#7), 2018-09-08.
+git checkout 57c0d083ee8ce6d5c9b417d88aa80a1c8d3d6419
+cd ..
+cd ..
+
 python -m pip install -r requirements.txt
 
 python tools\pack.py
