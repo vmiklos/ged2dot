eb1c16e56a28ddb3aa22525708098ad2d6d31c69
Miklos Vajna <vmiklos@vmiklos.hu>
2021-02-26 23:16:29 +0100

    pyinstaller: only bundle an explicit list of binaries
    Change-Id: I044b8f2b22ce626569e52eac0355988d37348b0e

--- pyi/hook-pygraphviz.py
+++ pyi/hook-pygraphviz.py
@@ -11,12 +11,30 @@ from PyInstaller.compat import is_win, is_darwin
 binaries = []
 datas = []
 
+# List of binaries agraph.py may invoke.
+progs = [
+    "neato",
+    "dot",
+    "twopi",
+    "circo",
+    "fdp",
+    "nop",
+    "acyclic",
+    "gvpr",
+    "gvcolor",
+    "ccomps",
+    "sccmap",
+    "tred",
+    "sfdp",
+    "unflatten",
+]
+
 if is_darwin:
     # The dot binary in PATH is typically a symlink, handle that.
     # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
     graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
-    for binary in glob.glob(graphviz_bindir + "/*"):
-        binaries.append((binary, "."))
+    for binary in progs:
+        binaries.append((graphviz_bindir + "/" + binary, "."))
     # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/lib/graphviz
     graphviz_libdir = os.path.realpath(graphviz_bindir + "/../lib/graphviz")
     for binary in glob.glob(graphviz_libdir + "/*.dylib"):
@@ -25,8 +43,8 @@ if is_darwin:
         datas.append((data, "graphviz"))
 
 if is_win:
-    for binary in glob.glob("c:/Program Files/Graphviz*/bin/*.exe"):
-        binaries.append((binary, "."))
+    for binary in progs:
+        binaries.append(("c:/Program Files/Graphviz*/bin/" + binary + ".exe", "."))
     for binary in glob.glob("c:/Program Files/Graphviz*/bin/*.dll"):
         binaries.append((binary, "."))
     for data in glob.glob("c:/Program Files/Graphviz*/bin/config*"):
