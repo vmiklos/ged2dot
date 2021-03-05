d0409880bb2c957432e0a595a84b799f4126bb83
Miklos Vajna <vmiklos@vmiklos.hu>
2021-03-05 22:58:13 +0100

    Add Linux support to hook-pygraphviz.py
    Change-Id: I1fd179082c71e0bd040dc0e65733e5a6c8a56dfd

--- pyi/hook-pygraphviz.py
+++ pyi/hook-pygraphviz.py
@@ -29,19 +29,6 @@ progs = [
     "unflatten",
 ]
 
-if is_darwin:
-    # The dot binary in PATH is typically a symlink, handle that.
-    # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
-    graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
-    for binary in progs:
-        binaries.append((graphviz_bindir + "/" + binary, "."))
-    # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/lib/graphviz
-    graphviz_libdir = os.path.realpath(graphviz_bindir + "/../lib/graphviz")
-    for binary in glob.glob(graphviz_libdir + "/*.dylib"):
-        binaries.append((binary, "graphviz"))
-    for data in glob.glob(graphviz_libdir + "/config*"):
-        datas.append((data, "graphviz"))
-
 if is_win:
     for prog in progs:
         for binary in glob.glob("c:/Program Files/Graphviz*/bin/" + prog + ".exe"):
@@ -50,5 +37,22 @@ if is_win:
         binaries.append((binary, "."))
     for data in glob.glob("c:/Program Files/Graphviz*/bin/config*"):
         datas.append((data, "."))
+else:
+    # The dot binary in PATH is typically a symlink, handle that.
+    # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
+    graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
+    for binary in progs:
+        binaries.append((graphviz_bindir + "/" + binary, "."))
+    # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/lib/graphviz
+    graphviz_libdir = os.path.realpath(graphviz_bindir + "/../lib/graphviz")
+    if is_darwin:
+        suffix = "dylib"
+    else:
+        suffix = "so"
+    for binary in glob.glob(graphviz_libdir + "/*." + suffix):
+        binaries.append((binary, "graphviz"))
+    for data in glob.glob(graphviz_libdir + "/config*"):
+        datas.append((data, "graphviz"))
+
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
