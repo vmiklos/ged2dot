dceb8d25a5729e1495949c1879eaa234a5882e44
Miklos Vajna <vmiklos@vmiklos.hu>
2021-03-05 23:24:19 +0100

    CI: add Windows error handling
    Which points out wixtools is not a valid package name.
    
    <https://stackoverflow.com/questions/9948517/how-to-stop-a-powershell-script-on-the-first-error>
    says there is no easy 'bash -e' equivalent, so this do this manually.
    
    Change-Id: I708d56c0cb3b9a8f1f3172616cd13c40f6c4eb44

--- tools/ci-build.ps1
+++ tools/ci-build.ps1
@@ -9,23 +9,31 @@
 #
 
 choco install graphviz
+if (-not $?) { throw "error $?" }
 # Register plugins.
 dot -c
-choco install wixtools
+if (-not $?) { throw "error $?" }
+choco install wixtoolset
+if (-not $?) { throw "error $?" }
 
 cd tools
 git clone https://github.com/jpakkane/msicreator
+if (-not $?) { throw "error $?" }
 cd msicreator
 # Allow to add extra attributes to MajorUpgrade node. (#7), 2018-09-08.
 git checkout 57c0d083ee8ce6d5c9b417d88aa80a1c8d3d6419
+if (-not $?) { throw "error $?" }
 cd ..
 cd ..
 
 # Allow both 'graphviz' and 'Graphviz <version>'.
 $GVPATH = Resolve-Path "C:/Program Files/Graphviz*" | Select -ExpandProperty Path
 pip install --global-option=build_ext --global-option="-I${GVPATH}/include" --global-option="-L${GVPATH}/lib/" pygraphviz==1.6
+if (-not $?) { throw "error $?" }
 python -m pip install -r requirements.txt
+if (-not $?) { throw "error $?" }
 
 python tools\pack.py
+if (-not $?) { throw "error $?" }
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
