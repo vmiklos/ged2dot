vmiklos/ged2dot

commit 1f1c3755f07cb9b971d10dda93f1819269f6c63f

Update pylint to 2.12.2
Change-Id: Ib6d19605a7cbb560f135aed2b5c00b253acae1f9

qged2dot.py
@@ -75,7 +75,7 @@ class Widgets:
                 help_string += "-"
                 if node.wife and node.wife.get_surname():
                     help_string += node.wife.get_surname()
-                key = "%s (%s)" % (node.get_identifier(), help_string)
+                key = f"%{node.get_identifier()} ({help_string})"
                 self.rootfamily_value.addItem(key, node.get_identifier())
             self.update_status()
         except Exception:  # pylint: disable=broad-except

ged2dot.py
@@ -269,7 +269,7 @@ class Individual(Node):
                 sex = self.get_sex().lower()
             else:
                 sex = 'u'
-            image_path = get_abspath("placeholder-%s.svg" % sex)
+            image_path = get_abspath(f"placeholder-{sex}.svg")
         if basepath:
             image_path = os.path.relpath(image_path, basepath)
         label = "<table border=\"0\" cellborder=\"0\"><tr><td>"
@@ -496,7 +496,7 @@ class GedcomImport:
                 self.__handle_level1(rest)
             elif level == 2:
                 if rest.startswith("DATE"):
-                    year = rest.split(' ')[-1]
+                    year = rest.rsplit(' ', maxsplit=1)[-1]
                     if self.individual:
                         if self.in_birt:
                             self.individual.get_config().set_birth(year)

inlineize.py
@@ -9,27 +9,28 @@
 
 import base64
 import sys
-import xml.etree.ElementTree as ElementTree
+from xml.etree import ElementTree
 from typing import Union
 from typing import IO
 
-NAMESPACES = {
-    'svg': 'http://www.w3.org/2000/svg',
-    'xlink': 'http://www.w3.org/1999/xlink'
-}
+SVG_NS = 'http://www.w3.org/2000/svg'
+XLINK_NS = 'http://www.w3.org/1999/xlink'
 
 
 def inlineize(from_path: Union[str, IO[bytes]], to_path: Union[str, IO[bytes]]) -> None:
     """API interface to this module."""
-    ElementTree.register_namespace('', NAMESPACES['svg'])
-    ElementTree.register_namespace('xlink', NAMESPACES['xlink'])
+    ElementTree.register_namespace('', SVG_NS)
+    ElementTree.register_namespace('xlink', XLINK_NS)
     tree = ElementTree.ElementTree()
     tree.parse(from_path)
-    for image in tree.findall('.//{%s}image' % NAMESPACES['svg']):
-        xlinkhref = '{%s}href' % NAMESPACES['xlink']
+    svg_ns = "{" + SVG_NS + "}"
+    xlink_ns = "{" + XLINK_NS + "}"
+    for image in tree.findall(f".//{svg_ns}image"):
+        xlinkhref = f"{xlink_ns}href"
         href = image.attrib[xlinkhref]
         with open(href, 'rb') as stream:
-            image.attrib[xlinkhref] = "data:image/png;base64,%s" % base64.b64encode(stream.read()).decode('ascii')
+            b64 = base64.b64encode(stream.read()).decode('ascii')
+            image.attrib[xlinkhref] = f"data:image/png;base64,{b64}"
     tree.write(to_path)
 
 

libreoffice/base.py
@@ -25,7 +25,7 @@ class GedcomBase:
 
     def create_uno_service(self, name: str) -> Any:
         """Creates an UNO object instalce with the given name."""
-        return self.context.ServiceManager.createInstanceWithContext("com.sun.star.%s" % name, self.context)
+        return self.context.ServiceManager.createInstanceWithContext(f"com.sun.star.{name}", self.context)
 
     @staticmethod
     def to_dict(args: Iterable[Any]) -> Dict[str, Any]:
@@ -55,7 +55,7 @@ class GedcomBase:
             directory = os.path.dirname(path)
             if not os.path.exists(directory):
                 os.makedirs(directory)
-            with open(path, "a") as stream:
+            with open(path, "a", encoding="utf-8") as stream:
                 traceback.print_exc(file=stream)
         else:
             traceback.print_exc(file=sys.stderr)

libreoffice/dialog.py
@@ -54,7 +54,7 @@ class GedcomDialog(unohelper.Base, XPropertyAccess, XExecutableDialog, XImporter
             help_string += "-"
             if node.wife and node.wife.get_surname():
                 help_string += node.wife.get_surname()
-            key = "%s (%s)" % (node.get_identifier(), help_string)
+            key = f"{node.get_identifier()} ({help_string})"
             self.family_dict[key] = node
 
     def __create_control(self, options: Dict[str, Any]) -> Any:
@@ -63,7 +63,7 @@ class GedcomDialog(unohelper.Base, XPropertyAccess, XExecutableDialog, XImporter
         if "button_type" not in options:
             options["button_type"] = None
         parent = options["parent"]
-        control = parent.createInstance("com.sun.star.awt.UnoControl%sModel" % options["type_string"])
+        control = parent.createInstance(f"com.sun.star.awt.UnoControl{options['type_string']}Model")
         control.PositionX = options["left"]
         control.PositionY = options["top"]
         control.Width = options["width"]

libreoffice/importer.py
@@ -44,12 +44,12 @@ class GedcomImport(unohelper.Base, XFilter, XImporter, XExtendedFilterDetection,
         if sys.platform.startswith("win"):
             pattern = os.environ['PROGRAMFILES'] + '\\Graphviz*\\bin\\dot.exe'
             dot_paths = glob.glob(pattern)
-            if not dot_paths and 'PROGRAMFILES(x86)' in os.environ.keys():
+            if not dot_paths and 'PROGRAMFILES(x86)' in os.environ:
                 pattern = os.environ['PROGRAMFILES(x86)'] + '\\Graphviz*\\bin\\dot.exe'
                 dot_paths = glob.glob(pattern)
             if not dot_paths:
                 url = "<https://graphviz.gitlab.io/_pages/Download/Download_windows.html>"
-                raise Exception("No dot.exe found at '%s', please download it from %s." % (pattern, url))
+                raise Exception(f"No dot.exe found at '{pattern}', please download it from {url}.")
             dot_path = dot_paths[-1]
         else:
             dot_path = "dot"
@@ -71,14 +71,11 @@ class GedcomImport(unohelper.Base, XFilter, XImporter, XExtendedFilterDetection,
         root_family = "F1"
         layout_max_depth = "4"
         name_order = "little"
-        if "FilterData" in self.props.keys():
+        if "FilterData" in self.props:
             filter_data = self.to_dict(self.props["FilterData"])
-            if "rootfamily" in filter_data.keys():
-                root_family = filter_data["rootfamily"]
-            if "familydepth" in filter_data.keys():
-                layout_max_depth = filter_data["familydepth"]
-            if "nameorder" in filter_data.keys():
-                name_order = filter_data["nameorder"]
+            root_family = filter_data.get("rootfamily", root_family)
+            layout_max_depth = filter_data.get("familydepth", layout_max_depth)
+            name_order = filter_data.get("nameorder", name_order)
         config = {
             'input': ged,
             'rootfamily': root_family,

tests/test_ged2dot.py
@@ -138,7 +138,7 @@ class TestMain(unittest.TestCase):
         self.assertFalse(os.path.exists(config["output"]))
         ged2dot.convert(config)
         self.assertTrue(os.path.exists(config["output"]))
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             self.assertIn("images/", stream.read())
 
     def test_image_abspath(self) -> None:
@@ -155,7 +155,7 @@ class TestMain(unittest.TestCase):
         self.assertFalse(os.path.exists(config["output"]))
         ged2dot.convert(config)
         self.assertTrue(os.path.exists(config["output"]))
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             self.assertIn(config["imagedir"], stream.read())
 
     def test_config(self) -> None:
@@ -186,7 +186,7 @@ class TestMain(unittest.TestCase):
         self.assertFalse(os.path.exists(config["output"]))
         ged2dot.convert(config)
         self.assertTrue(os.path.exists(config["output"]))
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             self.assertNotIn("images/", stream.read())
 
     def test_bom(self) -> None:
@@ -457,7 +457,7 @@ class TestMain2(unittest.TestCase):
         self.assertFalse(os.path.exists(config["output"]))
         ged2dot.convert(config)
         self.assertTrue(os.path.exists(config["output"]))
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             self.assertNotIn('src="/', stream.read())
 
     def test_config_relpath_custom(self) -> None:
@@ -482,7 +482,7 @@ class TestMain2(unittest.TestCase):
         self.assertFalse(os.path.exists(config["output"]))
         ged2dot.convert(config)
         self.assertTrue(os.path.exists(config["output"]))
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             self.assertIn("† Y", stream.read())
 
     def test_config_nameorder_custom(self) -> None:
@@ -532,7 +532,7 @@ class TestMain2(unittest.TestCase):
 
         # Then make sure that explicit width and height is specified for the table around the image,
         # required by the PNG output:
-        with open(config["output"], "r") as stream:
+        with open(config["output"], "r", encoding="utf-8") as stream:
             graph = pygraphviz.AGraph(string=stream.read())
         family = graph.get_node("F1")
         stream = io.StringIO(family.attr.get("label"))

tests/test_inlineize.py
@@ -16,14 +16,14 @@ class TestMain(unittest.TestCase):
     """Tests main()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with open("tests/linked.svg", "r") as stream:
+        with open("tests/linked.svg", "r", encoding="utf-8") as stream:
             buffer = stream.read()
             self.assertIn("xlink:href=\"tests/images", buffer)
             self.assertNotIn("xlink:href=\"data:image/png;base64,", buffer)
         argv = ["", "tests/linked.svg", "tests/inline.svg"]
         with unittest.mock.patch('sys.argv', argv):
             inlineize.main()
-        with open("tests/inline.svg", "r") as stream:
+        with open("tests/inline.svg", "r", encoding="utf-8") as stream:
             buffer = stream.read()
             self.assertNotIn("xlink:href\"tests/images", buffer)
             self.assertIn("xlink:href=\"data:image/png;base64,", buffer)

commit a8503f6cea25045e30fe1c14d2875c4826f7ec90

qged2dot: fix typo, % was unwanted before F<num>
Regression from commit 1f1c3755f07cb9b971d10dda93f1819269f6c63f (Update
pylint to 2.12.2, 2021-12-22).

Change-Id: I94d3492ffe84aa742c43054ff0da8d44b743fa11

qged2dot.py
@@ -75,7 +75,7 @@ class Widgets:
                 help_string += "-"
                 if node.wife and node.wife.get_surname():
                     help_string += node.wife.get_surname()
-                key = f"%{node.get_identifier()} ({help_string})"
+                key = f"{node.get_identifier()} ({help_string})"
                 self.rootfamily_value.addItem(key, node.get_identifier())
             self.update_status()
         except Exception:  # pylint: disable=broad-except